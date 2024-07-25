9927793c8e684c4e8b5e66e2cdce347be48f714b
aharper2568 <anthonyharper568@gmail.com>
2024-07-25 17:13:15 -0400

    added schema.sql to create database and tables

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
+
+
+\c department_db;
+
+
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
+
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+);
